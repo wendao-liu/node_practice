69292f337a91f258d79fdef99b762eeff9095384
wb-lhy498637 <wb-lhy498637@alibaba-inc.com>
2020-09-22 19:17:58 +0800

    完成第一天作业

--- ex01/__tests__/index.spec.js
+++ ex01/__tests__/index.spec.js
@@ -1,24 +1,22 @@
 test('练习01 自动化生成路由配置',() => {
     const {getRouter} = require('../index')
     const ret = getRouter(__dirname + '/data')
-    expect(getRouter(__dirname + '/data')).toBe(
-`
-export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-{
-    path: '/about',
-    name: 'about',
-    component: () => import('./views/about.vue')
-},
-{
-    path: '/index',
-    name: 'index',
-    component: () => import('./views/index.vue')
-},
-
-    ]
-})`
+    console.log(ret,'ret')
+    expect(ret).toBe(
+        `export default new Router({
+            mode: 'history',
+            base: process.env.BASE_URL,
+            routes: [
+                {
+                        paht: '/about',
+                        name: 'about',
+                        component: () => import(./views/about.vue)
+                    }{
+                        paht: '/index',
+                        name: 'index',
+                        component: () => import(./views/index.vue)
+                    }
+            ]
+        })`
     )
 })
